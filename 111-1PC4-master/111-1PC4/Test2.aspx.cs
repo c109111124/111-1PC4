2dde9a82e1830f87f5b9bc715a20aa1a6442d0c5
c109111124 <100175502+c109111124@users.noreply.github.com>
2022-09-22 17:58:25 +0800

    Add files via upload

---
+++ 111-1PC4-master/111-1PC4/Test2.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1PC4
+{
+    public partial class Test2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int i_Age = 49; int i_Sex = 0;
+            if (!(!(i_Age >= 50) || !(i_Sex == 1)))
+            {
+                Response.Write("壯年男人的程式執行程式碼區域");
+            }
+            else if (!(!(i_Age < 50) || !(i_Sex == 1)))
+            {
+                Response.Write("年輕男人的程式執行程式碼區域");
+            }
+            else if (!(!(i_Age >= 50) || !(i_Sex == 0)))
+            {
+                Response.Write("壯年女人的程式執行程式碼區域");
+            }
+            else if (!(!(i_Age < 50) || !(i_Sex == 0)))
+            {
+                Response.Write("年輕女人的程式執行程式碼區域");
+            }
+            else
+            {
+                Response.Write("其他");
+            }
+        }
+    }
+}
